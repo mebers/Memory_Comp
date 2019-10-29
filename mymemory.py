00e4df822161431c6a3be6bd00ae40576d1816eb
Michael Ebersold <michael.ebersold@ligo.com>
2019-10-29 07:19:34 -0700

    Works, not taken much care of conventions

--- mymemory.py
+++ mymemory.py
@@ -40,12 +40,11 @@ def h_dom_mem(approximant, q, chi1, chi2, dt, M, dist_mpc, f_low,f_ref, phi_ref,
 
     return t, hmem
 
-def h_memory(approximant, q, chi1, chi2, dt, M, dist_mpc, f_low,f_ref, phi_ref,inclination):
+def h_memory20(approximant, q, chi1, chi2, dt, M, dist_mpc, f_low,f_ref, phi_ref,inclination):
     
     t, mode_dict = utils.generate_LAL_modes(approximant, q, chi1, chi2, dt, \
             M, dist_mpc, f_low, f_ref,phi_ref)  
 
-    #mode_dict_dot = np.gradient(mode_dict,dt)
     mode_dict.update({mode: np.gradient(mode_dict[mode],dt) for mode in mode_dict.keys()})
 
     dh20mem_p = np.zeros(len(t))
@@ -53,8 +52,9 @@ def h_memory(approximant, q, chi1, chi2, dt, M, dist_mpc, f_low,f_ref, phi_ref,i
     const = dist_mpc*10**6*utils.PC_SI/utils.C_SI*1./np.sqrt(24.)
     print(const)
     
-    for llp in range(2,5):
-        for llpp in range(2,5):
+    llmax = 4
+    for llp in range(2,llmax+1):
+        for llpp in range(2,llmax+1):
             for mp in range(-llp,llp+1):
                 for mpp in range(-llpp,llpp+1):
                     
@@ -63,7 +63,7 @@ def h_memory(approximant, q, chi1, chi2, dt, M, dist_mpc, f_low,f_ref, phi_ref,i
                     if G_ang_int(2,llp,llpp,0,mp,mpp)==0:
                         continue
                     
-                    start = timer()
+                    #start = timer()
                     hdotp = mode_dict['h_l%dm%d'%(llp, mp)]
                     hdotp_conj = np.conjugate(mode_dict['h_l%dm%d'%(llpp, mpp)])
 
@@ -73,9 +73,11 @@ def h_memory(approximant, q, chi1, chi2, dt, M, dist_mpc, f_low,f_ref, phi_ref,i
                     dh20mem_p = dh20mem_p + const*G_ang_int(2,llp,llpp,0,mp,mpp)*prod_hdot_p
                     dh20mem_c = dh20mem_c + const*G_ang_int(2,llp,llpp,0,mp,mpp)*prod_hdot_c
                     
-                    end = timer()
-                    print('Time for mode %d %d %d %d'%(llp,llpp,mp,mpp))
-                    print(end - start)
+                    #end = timer()
+                    #print('Time for mode %d %d %d %d'%(llp,llpp,mp,mpp))
+                    #print(end - start)
+                    print('Combination %d %d %d %d %d %d done'%(ll,llp,llpp,m,mp,mpp))
+
 
     
     h20mem_p = np.cumsum(dh20mem_p)*dt   
@@ -85,18 +87,64 @@ def h_memory(approximant, q, chi1, chi2, dt, M, dist_mpc, f_low,f_ref, phi_ref,i
 
     return t, hmem_p, hmem_c
 
-def testloop():
-    for llp in range(2,5):
-        for llpp in range(2,5):
-            for mp in range(-llp,llp+1):
-                for mpp in range(-llpp,llpp+1):
+
+def h_memory(approximant, q, chi1, chi2, dt, M, dist_mpc, f_low,f_ref, phi_ref,inclination):
+    
+    t, mode_dict = utils.generate_LAL_modes(approximant, q, chi1, chi2, dt, \
+            M, dist_mpc, f_low, f_ref,phi_ref)
+
+    mode_dict.update({mode: np.gradient(mode_dict[mode],dt) for mode in mode_dict.keys()})
+    
+    llmax = 4
+    memp_mode_dict = {}
+    memc_mode_dict = {}
+    
+    for ll in range(2,llmax+1):
+        const = dist_mpc*10**6*utils.PC_SI/utils.C_SI*np.sqrt(np.math.factorial(ll-2)/float(np.math.factorial(ll+2)))
+        print(const)
+        for m in range(-ll,ll+1):
+            dhmem_p = np.zeros(len(t))
+            dhmem_c = np.zeros(len(t))
+            
+            for llp in range(2,llmax+1):
+                for llpp in range(2,llmax+1):
+                    for mp in range(-llp,llp+1):
+                        for mpp in range(-llpp,llpp+1):
                     
-                    if mp != mpp:
-                        continue
-                    if G_ang_int(2,llp,llpp,0,mp,mpp)==0:
-                        continue
+                            if m != mp - mpp:
+                                continue
+                            if G_ang_int(ll,llp,llpp,m,mp,mpp)==0:
+                                continue
                     
-                    print(llp,llpp,mp,mpp)
+                            #start = timer()
+                            hdotp = mode_dict['h_l%dm%d'%(llp, mp)]
+                            hdotp_conj = np.conjugate(mode_dict['h_l%dm%d'%(llpp, mpp)])
+
+                            prod_hdot_p = np.array(np.real(hdotp*hdotp_conj))
+                            prod_hdot_c = np.array(np.imag(hdotp*hdotp_conj))
+
+                            dhmem_p = dhmem_p + const*G_ang_int(ll,llp,llpp,m,mp,mpp)*prod_hdot_p
+                            dhmem_c = dhmem_c + const*G_ang_int(ll,llp,llpp,m,mp,mpp)*prod_hdot_c
+                            
+                            #end = timer()
+                            #print('Time for mode %d %d %d %d %d %d'%(ll,llp,llpp,m,mp,mpp))
+                            #print(end - start)
+                            print('Combination %d %d %d %d %d %d done'%(ll,llp,llpp,m,mp,mpp))
+
+            memp_mode_dict['h_l%dm%d'%(ll, m)] = dhmem_p
+            memc_mode_dict['h_l%dm%d'%(ll, m)] = dhmem_c
+            
+    memp_mode_dict.update({mode: np.cumsum(memp_mode_dict[mode])*dt for mode in memp_mode_dict.keys()})
+    memc_mode_dict.update({mode: np.cumsum(memc_mode_dict[mode])*dt for mode in memc_mode_dict.keys()})
+    hmem_p = np.zeros(len(t))
+    hmem_c = np.zeros(len(t))
+    
+    for ll in range(2,llmax+1):
+        for m in range(-ll,ll+1):
+            hmem_p = hmem_p + memp_mode_dict['h_l%dm%d'%(ll, m)]*harmonics.sYlm(-2, ll, m, inclination, phi_ref)
+            hmem_c = hmem_c - memc_mode_dict['h_l%dm%d'%(ll, m)]*harmonics.sYlm(-2, ll, m, inclination, phi_ref)
+
+    return t, hmem_p, hmem_c
     
 
 # Generate a waveform
@@ -105,53 +153,29 @@ dist_mpc = 100.
 f_low = 20.
 f_ref = 20.
 
-q = 1.9
-chi1 = np.array([0.5, 0.3, 0.5])
-chi2 = np.array([-0.6, 0.2, 0.4])
+q = 3.2
+chi1 = np.array([-0.5, 0.4, 0.6])
+chi2 = np.array([0.6, -0.4, -0.4])
 M = 70.
 inclination = 2.*np.pi/5
 phi_ref = np.pi/4
 
 approximant = 'NRSur7dq4'
 
-"""
-t, mode_dict = utils.generate_LAL_modes(approximant, q, chi1, chi2, dt, \
-            M, dist_mpc, 0, f_ref, phi_ref=phi_ref)    
-h22 = mode_dict['h_l2m2']
-h22_dot = np.gradient(h22,dt)
-print(h22_dot*h22_dot*dist_mpc*10**6*utils.PC_SI)
-
-# time, h = h+ -1j * hx
-#for (key,values) in mode_dict.items() :
-#   print(key)
-t, h20 = h_dom_mem(approximant, q, [0., 0., 0.0], [0.0, 0.0, 0.0], dt, M, dist_mpc, 0,f_ref, phi_ref,inclination)
-plt.plot(t,np.real(h20),label='non-spinning, equal mass')
-
-t, h20 = h_dom_mem(approximant, 3.2, [0., 0., 0.0], [0.0, 0.0, 0.0], dt, M, dist_mpc, 0,f_ref, phi_ref,inclination)
-plt.plot(t,np.real(h20),label='non-spinning, q=3.2')
-
-t, h20 = h_dom_mem(approximant, q, [0., 0., 0.8], [0.0, 0.0, 0.8], dt, M, dist_mpc, 0,f_ref, phi_ref,inclination)
-plt.plot(t,np.real(h20),label='aligned spins, equal mass')
-
-t, h20 = h_dom_mem(approximant, q, chi1, chi2, dt, M, dist_mpc, 0,f_ref, phi_ref,inclination)
-plt.plot(t,np.real(h20),label='precessing, equal mass')
-
-t, h20 = h_dom_mem(approximant, 3.4, chi1, chi2, dt, M, dist_mpc, 0,f_ref, phi_ref,inclination)
-plt.plot(t,np.real(h20),label='precessing, q=3.4')
-"""
-
 
 t, hmemdom = h_dom_mem(approximant, q, chi1, chi2, dt, M, dist_mpc, 0,f_ref, phi_ref,inclination)
-tt, hmem, hmemc = h_memory(approximant, q, chi1, chi2, dt, M, dist_mpc, 0,f_ref, phi_ref,inclination)
+tt, hmem20, hmemc20 = h_memory(approximant, q, chi1, chi2, dt, M, dist_mpc, 0,f_ref, phi_ref,inclination)
+ttt, hmem, hmemc = h_memory(approximant, q, chi1, chi2, dt, M, dist_mpc, 0,f_ref, phi_ref,inclination)
 
 plt.plot(t,np.real(hmemdom),label='Dominant memory')
-plt.plot(tt,np.real(hmem),label='memory in h_plus')
-plt.plot(tt,np.real(hmemc),label='memory in h_cross')
+plt.plot(tt,hmem20,label=r'memory in $h_+^{20}$')
+plt.plot(tt,hmemc20,label=r'memory in $h_\times^{20}$')
+plt.plot(ttt,hmem,label=r'memory in $h_+$')
+plt.plot(ttt,hmemc,label=r'memory in $h_\times$')
 
-
-plt.tight_layout()
 plt.legend(loc=2)
 plt.xlabel(r'$t$')
 plt.ylabel(r'$h^\mathrm{mem}$')
-plt.savefig('memdom_vs_mem20.pdf')
+plt.tight_layout()
+plt.savefig('memory_generic.pdf')
 plt.close()
